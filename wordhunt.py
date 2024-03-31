9707e597b9b1d89b70afdc889b3c488f02fa814c
jyou106 <jessieyou525@gmail.com>
2024-03-31 17:29:36 -0400

    Update wordhunt.py

--- wordhunt.py
+++ wordhunt.py
@@ -1,8 +1,17 @@
 """Word hunt simulator"""
 
-__author__ = ""
+from string import ascii_uppercase
+from random import choice
+
+__author__ = "Jessie You"
+
+SIZE = 4
+BOARD = [["A"]*SIZE for i in range(SIZE)]
 
 def main() -> None:
+    pass
+
+def randomized() -> None:
     pass
 
 if __name__ == "__main__":
