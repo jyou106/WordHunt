664a4d3aacb51c557ccc4d873665a04fe1f639e2
Jesse Wei <jesse@cs.unc.edu>
2024-03-31 20:28:54 -0400

    Nitpicks

--- wordhunt.py
+++ wordhunt.py
@@ -5,7 +5,11 @@ from random import choice
 import pprint
 import nltk
 from nltk.corpus import words
-nltk.download('words')
+
+try:
+    words.words()
+except LookupError:
+    nltk.download("words")
 
 __author__ = "Jessie You"
 
@@ -25,7 +29,7 @@ def play() -> None:
     pass
 
 def is_word_valid(word: str) -> bool:
-    return word in WORDS
+    return word.lower() in WORDS
 
 def randomized() -> None:
     for i in range(len(BOARD)):
@@ -36,26 +40,31 @@ def pretty_print() -> None:
     pprint.pprint(BOARD)
 
 def get_input() -> tuple[int, int]:
-    x: str
-    valid: bool = False
-    while not valid:
-        valid = True
-        x = input("Input: ")
-        x = x.split(",")
-        if len(x) != 2:
-            print("Invalid input. Ex: 0,0")
-            valid = False
-        for char in x:
-            if not char.isdigit() or int(char) < 0:
-                print("Should contain only nonnegative digits")
-                valid = False     
+    indices: tuple[int, ...]
+    while True:
+        user_input = input("Input: ")
+        user_input = user_input.replace(" ", "")
 
-    return (int(x[0]), int(x[1]))
+        if not all(char.isdigit() or char == "," for char in user_input):
+            print("Invalid input. Ex: 0, 0 (nonnegative integers)")
+            continue
+
+        indices = tuple(map(int, user_input.split(",")))
+
+        if len(indices) != 2:
+            print("Invalid number of inputs. Ex: 0, 0")
+            continue
+        if indices[0] >= SIZE or indices[1] >= SIZE:
+            print(f"Invalid input. Both indices must be less than {SIZE}")
+            continue
+
+        break
+
+    return indices[0], indices[1]
 
 def is_adjacent(left: tuple[int,int], right: tuple[int,int]) -> bool:
-    if abs(left[0] - right[0]) <= 1:
-        if abs(left[1] - right[1]) <= 1:
-            return True
+    if abs(left[0] - right[0]) <= 1 and abs(left[1] - right[1]) <= 1:
+        return True
     return False
 
 if __name__ == "__main__":
