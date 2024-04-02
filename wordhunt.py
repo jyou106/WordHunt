48d5998235bf4a8685744d2854c1397c0824953e
Jessie You <147270998+jyou106@users.noreply.github.com>
2024-04-02 02:28:18 -0400

    Sorting and points (#7)
    * Add sorting and point counter features
    
    * Format code

--- wordhunt.py
+++ wordhunt.py
@@ -5,6 +5,7 @@ from random import choice
 import pprint
 import time
 import enchant
+import json
 
 
 __author__ = "Jessie You"
@@ -12,31 +13,58 @@ __author__ = "Jessie You"
 SIZE = 4
 BOARD = [["A"] * SIZE for i in range(SIZE)]
 WORDS = enchant.Dict("en_US")
+SCORES: dict[int, int] = {
+    3: 100,
+    4: 400,
+    5: 800,
+    6: 1400,
+    7: 1800,
+    8: 2200,
+    9: 2600,
+    10: 3000,
+    11: 3400,
+    12: 3800,
+    13: 4200,
+    14: 4600,
+    15: 5000,
+    16: 5400,
+}
 
 
 def main() -> None:
     randomized()
-    pretty_print()
-    play() 
+    play()
 
 
 def play() -> None:
     """Play the game"""
     # TODO
-    points: int
+    points: int = 0
     user_words: list[str] = []
-    duration = 10
+    duration = 20
     start_time = time.time()
     while int(time.time() - start_time) < duration:
-        word: str = get_word()
-        print(word)
-        if is_word_valid(word):
+        pprint.pprint(BOARD)
+        word: str | None = get_word()
+        if word is not None and is_word_valid(word):
             user_words.append(word)
+
+    sorted_words: list[str] = sort_words(user_words)
+    print(json.dumps(gen_words_and_scores(sorted_words), indent=4))
+
+
+def sort_words(words: list[str]) -> list[str]:
+    return sorted(words, key=lambda x: (len(x), x), reverse=True)
+
+
+def points(word: str) -> int:
+    return SCORES[len(word)]
 
 
 def is_word_valid(word: str) -> bool | None:
     """Return type includes None because `check` could raise error, but this will realistically never happen"""
-    # check does not care about case
+    if len(word) < 3:
+        return False
     return WORDS.check(word)
 
 
@@ -46,8 +74,8 @@ def randomized() -> None:
             BOARD[i][j] = choice(ascii_uppercase)
 
 
-def pretty_print() -> None:
-    pprint.pprint(BOARD)
+def gen_words_and_scores(words: list[str]) -> dict[str, int]:
+    return {word: points(word) for word in words}
 
 
 def get_word() -> str | None:
@@ -57,7 +85,10 @@ def get_word() -> str | None:
     user_input: str = input("Input: ")
     user_input.replace(" ", "")
 
-    if not all(char.isdigit() or char in {",", ";"} for char in user_input) or not user_input:
+    if (
+        not all(char.isdigit() or char in {",", ";"} for char in user_input)
+        or not user_input
+    ):
         print("Invalid input. Ex: 0,0;1,2;1,3 (nonnegative integers)")
         return None
 
@@ -67,25 +98,32 @@ def get_word() -> str | None:
     for index_pair in user_input_split:
         index_pair_int = tuple(map(int, index_pair.split(",")))
         if len(index_pair_int) != 2:
-            print(f"{index_pair_int} doesn't have 2 elements. Need pairs of indices (i, j).")
+            print(
+                f"{index_pair_int} doesn't have 2 elements. Need pairs of indices (i, j)."
+            )
             return None
         if index_pair_int[0] >= SIZE or index_pair_int[1] >= SIZE:
-            print(f"{index_pair_int} has out-of-bounds element(s). Both indices must be less than {SIZE}")
+            print(
+                f"{index_pair_int} has out-of-bounds element(s). Both indices must be less than {SIZE}"
+            )
             return None
         index_pairs.append(index_pair_int)
 
     # Should use iterator
     for i in range(0, len(index_pairs) - 1):
         prev_index_pair: tuple[int, int] = index_pairs[i]
-        curr_index_pair: tuple[int, int] = index_pairs[i+1]
+        curr_index_pair: tuple[int, int] = index_pairs[i + 1]
         if not is_adjacent(prev_index_pair, curr_index_pair):
-            print(f"Index pairs {prev_index_pair} and {curr_index_pair} are not adjacent to each other")
+            print(
+                f"Index pairs {prev_index_pair} and {curr_index_pair} are not adjacent to each other"
+            )
             return None
 
     for index_pair in index_pairs:
         s += BOARD[index_pair[0]][index_pair[1]]
-    
+
     return s
+
 
 def is_adjacent(left: tuple[int, int], right: tuple[int, int]) -> bool:
     if left == right:
