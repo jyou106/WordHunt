bbd939d253b9f860e2f73f175efd2d226da9b7aa
jyou106 <jessieyou525@gmail.com>
2024-04-02 02:22:02 -0400

    Format code

--- wordhunt.py
+++ wordhunt.py
@@ -27,13 +27,13 @@ SCORES: dict[int, int] = {
     13: 4200,
     14: 4600,
     15: 5000,
-    16: 5400
+    16: 5400,
 }
 
 
 def main() -> None:
     randomized()
-    play() 
+    play()
 
 
 def play() -> None:
@@ -54,7 +54,7 @@ def play() -> None:
 
 
 def sort_words(words: list[str]) -> list[str]:
-    return sorted(words, key=lambda x: (len(x), x), reverse = True)
+    return sorted(words, key=lambda x: (len(x), x), reverse=True)
 
 
 def points(word: str) -> int:
@@ -85,7 +85,10 @@ def get_word() -> str | None:
     user_input: str = input("Input: ")
     user_input.replace(" ", "")
 
-    if not all(char.isdigit() or char in {",", ";"} for char in user_input) or not user_input:
+    if (
+        not all(char.isdigit() or char in {",", ";"} for char in user_input)
+        or not user_input
+    ):
         print("Invalid input. Ex: 0,0;1,2;1,3 (nonnegative integers)")
         return None
 
@@ -95,24 +98,30 @@ def get_word() -> str | None:
     for index_pair in user_input_split:
         index_pair_int = tuple(map(int, index_pair.split(",")))
         if len(index_pair_int) != 2:
-            print(f"{index_pair_int} doesn't have 2 elements. Need pairs of indices (i, j).")
+            print(
+                f"{index_pair_int} doesn't have 2 elements. Need pairs of indices (i, j)."
+            )
             return None
         if index_pair_int[0] >= SIZE or index_pair_int[1] >= SIZE:
-            print(f"{index_pair_int} has out-of-bounds element(s). Both indices must be less than {SIZE}")
+            print(
+                f"{index_pair_int} has out-of-bounds element(s). Both indices must be less than {SIZE}"
+            )
             return None
         index_pairs.append(index_pair_int)
 
     # Should use iterator
     for i in range(0, len(index_pairs) - 1):
         prev_index_pair: tuple[int, int] = index_pairs[i]
-        curr_index_pair: tuple[int, int] = index_pairs[i+1]
+        curr_index_pair: tuple[int, int] = index_pairs[i + 1]
         if not is_adjacent(prev_index_pair, curr_index_pair):
-            print(f"Index pairs {prev_index_pair} and {curr_index_pair} are not adjacent to each other")
+            print(
+                f"Index pairs {prev_index_pair} and {curr_index_pair} are not adjacent to each other"
+            )
             return None
 
     for index_pair in index_pairs:
         s += BOARD[index_pair[0]][index_pair[1]]
-    
+
     return s
 
 
